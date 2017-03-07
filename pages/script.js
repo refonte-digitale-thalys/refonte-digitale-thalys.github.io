2674b1fbe6d302a8672636cdfc5dbd8ad91b7c3b
Barnaby Brachamul <barnaby.brachamul@gmail.com>
2017-03-07 22:44:29 +0100

    ...

--- pages/script.js
+++ pages/script.js
@@ -1,18 +1,13 @@
 function vueSetup() {
 
-	window.store = new Vuex.Store({
-		state: {
-			'pages': pages,
-		}
-	})
-
 	// Recursive page tree
 	Vue.component('page-tree', {
 		props: ['pages'],
 		template: `
 			<div class="pages">
 				<div class="page" v-for="page in pages">
-					<span class="page__name" v-html="page.name"></span>
+					<input id="{{ _uid }}" class="page__checkbox" type="checkbox" />
+					<label for="{{ _uid }}" class="page__name" v-html="page.name"></label>
 					<page-tree v-if="page.pages" :pages="page.pages"></page-tree>
 				</div>
 			</div>
@@ -24,86 +19,10 @@ function vueSetup() {
 	var app = new Vue({
 		el: '.pages',
 		template: `
-			<div class="app" lang="fr">
-				<page-tree :pages="pages"></page-tree>
+			<div id="tree" class="app" lang="fr">
+				<page-tree :pages="tree"></page-tree>
 			</div>
-		`,
-		computed: {
-			pages () {
-				for (i in store.state.pages) {
-					pages[i] = pages[i].split('/')
-				}
-				pages = treefy(pages)
-				return pages
-			}
-		}
+		`
 	})
 
-}
-
-function onlyUnique(value, index, self) {
-	return self.indexOf(value) === index;
-}
-
-function branchify(pages) {
-
-	console.log('branchifying : ', pages)
-
-	var nodes = []
-
-	for (i in pages) { // get all different possible pages
-		var page = pages[i]
-		console.log(page)
-		nodes.push(page[0])
-	}
-
-	nodes = nodes.filter( onlyUnique ) // get the unique page names
-
-	new_pages = []
-
-	console.log('nodes : ', nodes)
-
-	for (i in nodes) {
-		var nodeName = nodes[i]
-		var nodePages = pages.filter(obj => obj[0] == nodeName)
-		for (i in nodePages) {
-			nodePages[i].splice(0,1)
-		}
-		if (!nodeName) {
-			nodeName = '/'
-			new_pages.push({'name': nodeName})
-		} else {
-			new_pages.push({'name': nodeName, 'pages': nodePages})
-		}
-	}
-
-	console.log('new pages : ')
-	for (i in new_pages) {
-		console.log('	', new_pages[i].name)
-	}
-	console.log(new_pages)
-	console.log('--------------------------------------------------')
-
-	return new_pages
-
-}
-
-function treefy(pages) {
-	var tree = branchify(pages)
-	for (i in tree) {
-		if (tree[i].hasOwnProperty('pages')) {
-			console.log('DEBUGGING')
-			console.log('test branchify : ', branchify(tree[i].pages))
-			console.log(tree[i])
-			tree[i]['pages'] = branchify(tree[i]['pages'])
-			console.log(tree[i])
-			console.log('--------------------------------------------------')
-//		for (i in tree[i].pages) {
-//			if (tree[i].pages[i].hasOwnProperty('pages')) {
-//				tree[i].pages[i].pages = branchify(tree[i].pages[i].pages)
-//			}
-//		}
-		}
-	}
-	return tree
 }
--- pages/treemaker.py
+++ pages/treemaker.py
@@ -1,37 +1,108 @@
+import json
 from pages_thalys_short import list_of_pages
 
 for index, page in enumerate(list_of_pages) :
-    list_of_pages[index] = page.split('/')
+	list_of_pages[index] = page.split('/')
+
+class Node(object):
+
+	def __init__(self, name, url=None, children=None):
+		if children != None and children.isArray() == False :
+			raise AssertionError
+		self.name = name
+		self.url = url
+		self.children = children
+
+	def __str__(self):
+		if self.count():
+			return '<Node : {} · {}>'.format(self.name, self.count())
+		else :
+			return '<Node: {}>'.format(self.name)
+
+
+	def __hash__(self): return hash(id(self))
+	def __eq__(self, x): return x is self
+	def __ne__(self, x): return x is not self
+
+	def count(self):
+		if self.children :
+			return len(self.children)
+		else :
+			return None
+
+
+def say(something):
+	print(something)
+	print()
 
 def branchify(pages):
 
-    nodes = set()
-    for page in pages :
-        nodes.add(page[0])
+	nodeset = set()
 
-    new_pages = []
+	for page in pages :
+		nodeset.add(page[0])
 
-    for node in nodes :
-        nodeName = node
-        print(list_of_pages)
-        nodePages = list(filter(lambda x: x[0] == nodeName, list_of_pages)) # make this into a function
-        for page in nodePages :
-            del page[0]
-        if nodeName :
-            new_pages.append({
-                'name': nodeName,
-                'pages': nodePages,
-                })
-        else :
-            new_pages.append({
-                'name': '/',
-                })
-    return new_pages
+	new_nodes = []
+
+	for node in nodeset :
+		new_nodes.append(Node(name=node))
+
+	for node in new_nodes :
+
+		if node.name == '/' :
+			node.children = False
+			node.url = node.name
+		else :
+			node.children = []
+			for page in pages :
+				if page[0] and page[0] == node.name :
+					if len(page) == 1 :
+						node.children.append('/') # no children nodes
+					else :
+						node.children.append(page[1:]) # a list of children nodes, with root node sliced out
+			if len(node.children) > 1 :
+				node.children = branchify(node.children)
+
+	return new_nodes
 
 #print(list_of_pages)
 
 list_of_pages = branchify(list_of_pages)
 
-#for index, page in enumerate(list_of_pages) :
-#    print(page)
-#    list_of_pages[index]['pages'] = branchify(page['pages'])
+def export_to_file() :
+	text_file = open("tree.js", "w")
+	text_file.write(
+		"window.tree = " + 
+		json.dumps(list_of_pages, ensure_ascii=False)
+		)
+	text_file.close()
+
+
+#def count_pages(nodes) :
+#	for node in nodes :
+#		number = 0
+#		if 'pages' in node :
+#			for page in node['pages'] :
+#				if page['name'] == '/' :
+#					number += 1
+#				else :
+#					page['pages'] = count_pages(page['pages'])
+#		node['count'] = number
+#	return nodes
+
+#say(list_of_pages)
+
+def node_count(node):
+	if 'count' in node :
+		return '(' + str(node['count']) + ')'
+	else :
+		return ''
+
+def print_tree(nodes, tabulation='') :
+	for node in nodes :
+		print(node)
+#		print('{}{} {}'.format(tabulation, node.name, node.count()))
+#		if node.children :
+#			print_tree(node.children, tabulation=tabulation+'----')
+
+print_tree(list_of_pages)
