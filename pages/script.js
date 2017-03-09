fa0140b4f4cd15ed325e2df7d15594674903b397
Brachamul <barnaby.brachamul@gmail.com>
2017-03-09 08:28:03 +0100

    tree !

--- pages/script.js
+++ pages/script.js
@@ -2,25 +2,39 @@ function vueSetup() {
 
 	// Recursive page tree
 	Vue.component('page-tree', {
-		props: ['pages'],
+		props: ['nodes'],
 		template: `
 			<div class="pages">
-				<div class="page" v-for="page in pages">
-					<input id="{{ _uid }}" class="page__checkbox" type="checkbox" />
-					<label for="{{ _uid }}" class="page__name" v-html="page.name"></label>
-					<page-tree v-if="page.pages" :pages="page.pages"></page-tree>
-				</div>
+				<page-branch v-for="node in nodes" :node="node"></page-branch>
 			</div>
 		`
 	})
 
+	Vue.component('page-branch', {
+		props: ['node'],
+		template: `
+			<div class="page">
+				<input v-if="node.hasOwnProperty('children')" :id="makeId" class="page__checkbox" type="checkbox" checked />
+				<label v-if="node.hasOwnProperty('children')" class="page__deploy" :for="makeId"></label>
+				<span class="page__name" v-html="node.name"></span>
+				<span class="page__count" v-if="node.count" v-html="node.count"></span>
+				<span v-if="node.adress"><a class="chevron" :href="'https://' + node.adress" target="_blank">></a></span>
+				<page-tree v-if="node.hasOwnProperty('children')" :nodes="node.children"></page-tree>
+			</div>
+		`,
+		computed: {
+			makeId: function() {
+				return Math.random().toString().slice(2,14)
+			},
+		}
+	})
 
 	// App
 	var app = new Vue({
 		el: '.pages',
 		template: `
 			<div id="tree" class="app" lang="fr">
-				<page-tree :pages="tree"></page-tree>
+				<page-tree :nodes="[tree]"></page-tree>
 			</div>
 		`
 	})
--- pages/treemaker.py
+++ pages/treemaker.py
@@ -1,24 +1,62 @@
 import json
-from pages_thalys_short import list_of_pages
+from pages_thalys import list_of_pages
 
 for index, page in enumerate(list_of_pages) :
 	list_of_pages[index] = page.split('/')
 
 class Node(object):
 
-	def __init__(self, name, url=None, children=None):
-		if children != None and children.isArray() == False :
-			raise AssertionError
+	def __init__(self, name, url=None, children=None, adress=''):
 		self.name = name
 		self.url = url
 		self.children = children
+		self.adress = adress
+
+	@property
+	def children(self):
+		return self._children
+
+	@children.setter
+	def children(self, x):
+		if x != None and type(x) != list :
+			raise Exception("Node children must be 'None' or List, not {}.".format(type(x)))
+		self._children = x
 
 	def __str__(self):
-		if self.count():
-			return '<Node : {} · {}>'.format(self.name, self.count())
-		else :
-			return '<Node: {}>'.format(self.name)
+		string = '<Node : {}'.format(self.name)
+		if self.url: string += ' · url("{}")'.format(self.url)
+		if self.count(): string += ' · {}'.format(self.count())
+		if self.children: string += ' · hasChildren'
+		string += '>'
+		return string
 
+	def tree(self, tabulation=''):
+		string = tabulation + '<Node : {}'.format(self.name)
+		if self.url: string += ' · "{}{}"'.format(self.adress, self.url)
+		if self.count(): string += ' · {}'.format(self.count())
+		string += '>'
+		if self.children:
+			string += '\n'
+			for node in self.children :
+				string += node.tree(tabulation=tabulation+'    ')
+		return string
+
+	def json_tree(self):
+		string = "{"
+		string += "'name': '{}'".format(self.name)
+		if self.url: string += ", 'adress': '{}{}'".format(self.adress, self.url)
+		if self.count(): string += ", 'count': {}".format(self.count())
+		if self.children:
+			string += ", 'children': ["
+			for node in self.children[:-1] :
+				string += node.json_tree() + ','
+			string += self.children[-1].json_tree()
+			string += ']'
+		string += '}'
+		return string
+
+	def __repr__(self):
+		return str(self)
 
 	def __hash__(self): return hash(id(self))
 	def __eq__(self, x): return x is self
@@ -35,7 +73,7 @@ def say(something):
 	print(something)
 	print()
 
-def branchify(pages):
+def branchify(pages, adress=''):
 
 	nodeset = set()
 
@@ -45,23 +83,23 @@ def branchify(pages):
 	new_nodes = []
 
 	for node in nodeset :
-		new_nodes.append(Node(name=node))
+		new_nodes.append(Node(name=node, adress=adress+'/'+node))
 
 	for node in new_nodes :
 
 		if node.name == '/' :
-			node.children = False
+			node.children = None
 			node.url = node.name
 		else :
 			node.children = []
 			for page in pages :
 				if page[0] and page[0] == node.name :
 					if len(page) == 1 :
-						node.children.append('/') # no children nodes
+						node.url = '/' # no children nodes
 					else :
 						node.children.append(page[1:]) # a list of children nodes, with root node sliced out
-			if len(node.children) > 1 :
-				node.children = branchify(node.children)
+			if len(node.children) > 0 :
+				node.children = branchify(node.children, adress=node.adress)
 
 	return new_nodes
 
@@ -69,14 +107,14 @@ def branchify(pages):
 
 list_of_pages = branchify(list_of_pages)
 
-def export_to_file() :
+def export_to_file(tree) :
 	text_file = open("tree.js", "w")
-	text_file.write(
-		"window.tree = " + 
-		json.dumps(list_of_pages, ensure_ascii=False)
-		)
+	text_file.write("window.tree = " + tree)
 	text_file.close()
 
+export_to_file(list_of_pages[0].json_tree())
+
+print(list_of_pages[0].json_tree())
 
 #def count_pages(nodes) :
 #	for node in nodes :
@@ -100,9 +138,11 @@ def node_count(node):
 
 def print_tree(nodes, tabulation='') :
 	for node in nodes :
-		print(node)
-#		print('{}{} {}'.format(tabulation, node.name, node.count()))
+#		print('Type : ', type(node))
+#		print('Children : ', node.children)
+		print()
+		print(node.json_tree())
 #		if node.children :
 #			print_tree(node.children, tabulation=tabulation+'----')
 
-print_tree(list_of_pages)
+#print_tree(list_of_pages)
