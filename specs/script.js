3dc4c8f869e24897b1ee1df737814a2cc6a595c8
Brachamul <barnaby.brachamul@gmail.com>
2017-04-10 20:36:20 +0200

    specs

---
+++ specs/script.js
@@ -0,0 +1,127 @@
+function vueSetup() {
+	// Store
+	window.store = new Vuex.Store({
+		state: {
+			'themes': false,
+			'epics': false,
+			'stories': false,
+			'comments': false
+		},
+		mutations: {
+			updateData(state, payload) {
+				for (param in payload) {
+					state[param] = payload[param]
+
+					if (state['stories'] && state['comments']) {
+						var stories = state['stories']
+						var comments = state['comments']
+						for (i in stories) {
+							var story = stories[i]
+
+							// Give a fake title to untitled stories
+							if (!story.Titre) story.Titre = "(Sans Titre)"
+
+							// 
+							for (i in story['Commentaires']) {
+								// for each story note, replace the note ID by the full note data
+								console.log('reading comments for a story')
+								story['Commentaires'][i] = comments.filter(obj => obj.airTableId  == story['Commentaires'][i] )[0]
+								if (story['Commentaires'][i].hasOwnProperty('Commentaire')) {
+									story['Commentaires'][i]['Commentaire'] = marked(story['Commentaires'][i]['Commentaire'])
+								}
+							}
+
+						}
+					}
+//
+//					if (state['stories'] && state['notes']) {
+//
+//						// once both stories and notes have been fetched, we match them
+//						var stories = state['stories']
+//						var notes = state['notes']
+//
+//						for (i in stories) {
+//							var story = stories[i]
+//							for (i in story.Notes) {
+//
+//								// for each story note, replace the note ID by the full note data
+//								story.Notes[i] = notes.filter(obj => obj.airTableId  == story.Notes[i] )[0]
+//								if (story.Notes[i].hasOwnProperty('Note')) {
+//									story.Notes[i].Note = marked(story.Notes[i].Note)
+//								}
+//			
+//							}
+//						}
+//
+//						// Ordonne les stories
+//						state.stories.sort(function(a, b){
+//							a = parseInt(a.Ordre)
+//							b = parseInt(b.Ordre)
+//							return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+//						})
+//
+//					}
+
+				}
+			}
+		}
+	})
+
+	var airTable = new getAirTable(apiKey='keyiXWAznJ80FXmtW', appKey='appguOHxX26IPEDZ9')
+
+	Vue.http.get(airTable.ListEndpoint('User Stories')).then((response) => {
+		var stories = airTable.Clean(response.body.records)
+		store.commit('updateData', {'stories': stories})
+	})
+
+	Vue.http.get(airTable.ListEndpoint('Commentaires Backlog')).then((response) => {
+		var comments = airTable.Clean(response.body.records)
+		store.commit('updateData', {'comments': comments})
+	})
+	
+	// App
+	var app = new Vue({
+		el: '#app',
+		store,
+		template: `
+
+			<div id="app" class="stories" lang="fr">
+
+				<div class="story" v-for="story in stories" v-if="!story.Hide">
+					<div class="story__container">
+						<h4>{{ story['Titre'] }}</h4>
+						<blockquote>
+							En tant qu'{{ story['En tant que'] }}, je veux
+							{{ story['Je veux'] }}
+							<span v-if="story['Pourquoi']">, </span>
+							<span v-else>.</span>
+							{{ story['Pourquoi'] }}.
+						</blockquote>
+						<div v-for="comment in story['Commentaires']">
+							<div v-html="comment.Commentaire"></div>
+							<div v-if="comment.Illustrations" class="illustrations">
+								<a
+									class="illustration"
+									v-for="illustration in comment.Illustrations]"
+									:href="illustration.url" target='_story'
+									>
+									<img class="illustration__image" :src="illustration.thumbnails.small.url" />
+								</a>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+
+			</div>
+		
+		`,
+		computed: {
+			stories () {
+				return this.$store.state.stories
+			}
+		}
+	})
+
+}
